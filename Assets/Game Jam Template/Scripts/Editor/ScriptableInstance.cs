65962ac09e4f1694756ff0abbfbe1560862a20ac
Bixing Xie <bixing.xie@nyu.edu>
2018-01-27 14:39:39 -0500

    update

---
+++ Assets/Game Jam Template/Scripts/Editor/ScriptableInstance.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class YourClassAsset
+{
+	[MenuItem("Assets/Create/FaceSO")]
+	public static void CreateAsset ()
+	{
+		ScriptableObjectUtility.CreateAsset<NewBehaviourScript> ();
+	}
+}
---
+++ Assets/MyScriptableObject.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class NewBehaviourScript : ScriptableObject {
+
+	public List<Vector3> Eye_Right; 
+	public List<Vector3> Eye_Left; 
+	public List<Vector3> EyeBrow_Right_1; 
+	public List<Vector3> EyeBrow_Right_2; 
+	public List<Vector3> EyeBrow_Right_3;
+	public List<Vector3> EyeBrow_Left_1;  
+	public List<Vector3> EyeBrow_Left_2; 
+	public List<Vector3> EyeBrow_Left_3;  
+	public List<Vector3> Mouth_1; 
+	public List<Vector3> Mouth_2; 
+	public List<Vector3> Mouth_3; 
+	public List<Vector3> Mouth_4; 
+	public List<Vector3> Mouth_5; 
+	public List<Vector3> Mouth_6; 
+
+	public List<Vector3> eye_right{
+		get{ return Eye_Right; }
+	}
+	public List<Vector3> eye_left{
+		get{ return Eye_Left; }
+	}
+
+	public List<Vector3> eyebrow_right_1{
+		get{ return EyeBrow_Right_1; }
+	}
+
+	public List<Vector3> eyebrow_right_2{
+		get{ return EyeBrow_Right_2; }
+	}
+
+	public List<Vector3> eyebrow_right_3{
+		get{ return EyeBrow_Right_3; }
+	}
+
+	public List<Vector3> eyebrow_left_1{
+		get{ return EyeBrow_Left_1; }
+	}
+
+	public List<Vector3> eyebrow_left_2{
+		get{ return EyeBrow_Left_2; }
+	}
+
+	public List<Vector3> eyebrow_left_3{
+		get{ return EyeBrow_Left_3; }
+	}
+
+	public List<Vector3> mouth_1{
+		get { return Mouth_1; }
+	}
+
+	public List<Vector3> mouth_2{
+		get { return Mouth_2; }
+	}
+
+	public List<Vector3> mouth_3{
+		get { return Mouth_3; }
+	}
+
+	public List<Vector3> mouth_4{
+		get { return Mouth_4; }
+	}
+
+	public List<Vector3> mouth_5{
+		get { return Mouth_5; }
+	}
+
+	public List<Vector3> mouth_6{
+		get { return Mouth_6; }
+	}
+
+}
